3484b231919e7194017e16266d8724d9fd088adf
Eliraz <zarile010@gmail.com>
2025-01-10 14:29:51 +0200

    added input validation

--- src/client.py
+++ src/client.py
@@ -7,11 +7,54 @@ from src.utils.logger import Logger
 from src.utils.validations import is_msg_valid, validate_msg
 
 
+def get_file_size():
+    file_size = input("File size: ")
+
+    if file_size.isdigit():
+        return int(file_size)
+    elif file_size[0:-1].isdigit():
+        size = int(file_size[0:-1])
+        match file_size[-1].upper():
+            case "G":
+                return size * 1024 * 1024 * 1024
+            case "M":
+                return size * 1024 * 1024
+            case "K":
+                return size * 1024
+            case 'B':
+                return size
+    elif file_size[0:-2].isdigit():
+        unit = file_size[-2:].upper()
+        size = int(file_size[0:2])
+
+        match unit:
+            case "GB":
+                return size * 1024 * 1024 * 1024
+            case "MB":
+                return size * 1024 * 1024
+            case "KB":
+                return size * 1024
+
+    Logger.warn("Invalid value. please provide a valid file size in GB/MB/KB/B")
+    return get_file_size()
+
+
+def get_int(msg):
+    value = input(msg)
+    if not value.isdigit():
+        Logger.warn("Invalid Value. Must be an integer.")
+        return get_int(msg)
+    return value
+
+
 def init():
     # Todo: input validation
-    file_size = input("File size: ")
-    tcp_connections = input("Number of TCP connections: ")
-    udp_connections = input("Number of UDP connections: ")
+    file_size = get_file_size()
+    tcp_connections = get_int("Number of TCP connections: ")
+    udp_connections = get_int("Number of UDP connections: ")
+    if not tcp_connections or not udp_connections or not file_size:
+        Logger.warn("Invalid inputs. please enter again.")
+        return init()
     return file_size, int(tcp_connections), int(udp_connections)
 
 
